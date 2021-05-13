fcc4c4e27de4915d5fda419497162e63cc441ac9
Farid <farid.hammouten@gmail.com>
2021-05-14 00:45:21 +0900

    Home Page

---
+++ src/views/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from '../../components/Banner'
+import Nav from '../../components/Nav'
+import './HomeScreen.css'
+
+const HomeScreen = () => {
+    return (
+        <div className="homeScreen">
+            <Nav />
+            <Banner />
+
+            {/* Rows */}
+        </div>
+    )
+}
+
+export default HomeScreen
